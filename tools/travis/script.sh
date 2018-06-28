33c0892e8f4c14f995b4fa955fd4bb1dedad5ddf
Mark Harfouche <mark.harfouche@gmail.com>
2018-06-28 11:37:19 -0700

    Try installing full requirements/doc.txt for osx

--- tools/travis/script.sh
+++ tools/travis/script.sh
@@ -52,11 +52,11 @@ echo Build or run examples
 # OSX on Travis can't install sphinx-gallery.
 # I think all it needs is scikit-learn from that requirements doc
 # to run the tests. See Issue #3084
-if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
-  pip install --retries 3 -q scikit-learn
-else
+# if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+#   pip install --retries 3 -q scikit-learn
+# else
   pip install --retries 3 -q -r ./requirements/docs.txt
-fi
+# fi
 
 if [[ "${BUILD_DOCS}" == "1" ]]; then
   export SPHINXCACHE=${HOME}/.cache/sphinx
