7d9c8ace2aafc5212529b94dd4bee1b75cd60f10
Mark Harfouche <mark.harfouche@gmail.com>
2019-07-13 10:00:17 -0400

    pooch, remove unused imports from data

--- skimage/data/__init__.py
+++ skimage/data/__init__.py
@@ -6,14 +6,13 @@ For more images, see
 
 """
 
-import os as _os
 import numpy as np
 
 from distutils.version import LooseVersion as Version
 import pooch
 
 from ..io import imread
-from .._shared._warnings import expected_warnings, warn
+from .._shared._warnings import warn
 from ..util.dtype import img_as_bool
 from ._binary_blobs import binary_blobs
 from ._detect import lbp_frontal_face_cascade_filename
